07f48b437ff9f754a341856caf65d66a0ae52e12
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-25 20:51:31 +0100

    Create url.py and add first views

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,5 +10,24 @@ class Product(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveBigIntegerField(blank = True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def save(self,*args,**kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args,**kwargs)
+
+    def __str__(self):
+        return " solled {} - {} items for {} ".format(self.product.name,self.quantity
+        ,self.total_price)
+
+
 
 
