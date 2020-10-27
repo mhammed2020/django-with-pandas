4de28c2ae89c6c78f020b9ea4c734574efc70ebd
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-27 17:16:11 +0100

    Add some helper function to plot data in utils.py

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 # Create your models here.
 
 class Product(models.Model):
@@ -17,7 +17,7 @@ class Purchase(models.Model):
     quantity = models.PositiveIntegerField()
     total_price = models.PositiveBigIntegerField(blank = True)
     salesman = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(default=timezone.now)
 
 
     def save(self,*args,**kwargs):
--- products/views.py
+++ products/views.py
@@ -1,25 +1,54 @@
 from django.shortcuts import render
 from . models import Product,Purchase
 import pandas as pd
+from .utils import get_simple_plot
+
+from django.http import HttpResponse
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request) :
+    graph = None
+    error_message = None
+    df = None
 
     qs = pd.DataFrame(Product.objects.all().values())
     qs1 = pd.DataFrame(Purchase.objects.all().values())
     qs['product_id'] = qs['id']
-    df = pd.merge(qs1,qs,on='product_id').drop(['id_y','date_y'],axis=1).rename({'id_x':'id','date_x':'date'},axis = 1)
 
-    if request.method =='POST' :
-        chart_type = request.POST['sales']
-        print(chart_type)
+    if qs1.shape[0] > 0 :
+
+        df = pd.merge(qs1,qs,on='product_id').drop(['id_y','date_y'],axis=1).rename({'id_x':'id','date_x':'date'},axis = 1)
+
+        if request.method =='POST' :
+            chart_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+            
+            df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+            df2 = df.groupby('date', as_index = False)['total_price'].agg('sum')
+
+            if chart_type != "":
+                if date_from != "" and date_to !="" :
+                    df = df[(df['date']>date_from) & (df['date']< date_to)]
+                    df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
+                graph = get_simple_plot(chart_type, x=df2['date'], y=df2['total_price'], data=df)
+            else:
+                error_message = "Please select a chart type to continue"
+          
+    else :
+        error_message = 'no records in the database'
+      
+
+
 
 
     context ={
-        'products':qs.to_html(),
-         'purchase':qs1.to_html(),
-
-         'df' : df.to_html
+        'graph': graph, 
+        'error_message' : error_message ,
+        
     }
 
     return render(request,'products/main.html',context)
