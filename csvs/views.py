775d03635fc55f12772d17bec8bbf8387a6f2e96
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-28 18:35:45 +0100

    upload csv file using external form

--- csvs/views.py
+++ csvs/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render
 
+from .forms import CsvForm
+from .models import Csv
+import csv
+from django.contrib.auth.models import User
+from products.models import Product, Purchase
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def upload_file_view(request):
     error_message = None
     success_message = None
+    form = CsvForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = CsvForm()
 
-    return render(request, 'csvs/upload.html', {})
+        # obj = Csv.objects.filter(activated=False) 
+        # with open(obj.file_name.path, 'r') as f:
+        #         reader = csv.reader(f)
+
+        #         for row in reader:
+        #             print(row)
+               
+
+        # obj.activated=True
+        # obj.save()
+        success_message= "Uploaded sucessfully"
+    context = {
+        'form': form,
+       
+    }
+    return render(request, 'csvs/upload.html', context)
 
 
 
