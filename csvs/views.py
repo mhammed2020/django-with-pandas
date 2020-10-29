01ca9d29e955ec417bf65011035266fe8fc8d0a7
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-29 13:20:27 +0100

    Prepare data set for our app csvs

--- csvs/views.py
+++ csvs/views.py
@@ -17,16 +17,32 @@ def upload_file_view(request):
         form.save()
         form = CsvForm()
 
-        # obj = Csv.objects.filter(activated=False) 
-        # with open(obj.file_name.path, 'r') as f:
-        #         reader = csv.reader(f)
+        obj = Csv.objects.get(activated=False) 
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
 
-        #         for row in reader:
-        #             print(row)
+            for row in reader:
+                
+                row = "".join(row)
+                row = row.replace(";", " ")
+               
+                row = row.split()
+                print(row)
+                # user = User.objects.get(id=row[1])
+                # print(user)
+                # prod, _ = Product.objects.get_or_create(name=row[0])
+                # Purchase.objects.create(
+                #         product=prod,
+                #         price = int(row[2]),
+                #         quantity = int(row[1]),
+                #         salesman = user,
+                #         date = row[4]+ " "+ row[5]
+                #     )
+               
                
 
-        # obj.activated=True
-        # obj.save()
+        obj.activated=True
+        obj.save()
         success_message= "Uploaded sucessfully"
     context = {
         'form': form,
