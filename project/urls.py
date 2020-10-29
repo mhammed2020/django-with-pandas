21ca5003526a813f3b1ca7ddce21c83058ea759a
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-10-29 17:34:43 +0100

    Add login feature

--- project/urls.py
+++ project/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from .views import home_view,login_view,logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('performance/', include('products.urls', namespace='products')),
     path('upload/', include('csvs.urls', namespace='csvs')),
     path('customers/', include('customers.urls', namespace='customers')),
